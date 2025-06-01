3bc6420aa302adf8d5ff67622b0b4512664b34ec
Sandun Perera <sanduntharaka9651@gmail.com>
2025-06-01 20:48:25 +0530

    edit api

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -8,24 +8,28 @@ const api = axios.create({
 });
 
 api.interceptors.request.use(
+    
   (config) => {
     const token = localStorage.getItem('2903|Um1jsOnfx0c18WETvMWwGx7OAUGRVj59hYSXlx0e3d857168');
 
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
-    
+
     return config;
   },
   (error) => Promise.reject(error)
 );
 
+
 api.interceptors.response.use(
   (response) => response,
+
   (error) => {
     if (error.response?.status === 401) {
       console.warn('2903|Um1jsOnfx0c18WETvMWwGx7OAUGRVj59hYSXlx0e3d857168');
     }
+
     return Promise.reject(error);
   }
 );
